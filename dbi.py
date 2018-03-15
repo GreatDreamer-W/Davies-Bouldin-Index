0edc3ad9b3af23d83e4f18c854d4b12b5584f43b
Yirui Wang <email@wangyirui.cn>
2018-03-15 20:15:48 +0800

    Final final version

--- dbi.py
+++ dbi.py
@@ -1,3 +1,13 @@
+"""The Davies–Bouldin index (DBI) (introduced by David L. Davies and 
+Donald W. Bouldin in 1979) is a metric for evaluating clustering algorithms. 
+This is an internal evaluation scheme, where the validation of how well the 
+clustering has been done is made using quantities and features inherent to the 
+dataset.
+
+This implementation just computes DBI of two clusters.
+"""
+
+
 import numpy as np
 
 
